a3dcd82fae5c173f2eb06e51ee889f2142b95bcd
ZTong1201 <buaatzy@163.com>
2019-09-17 11:40:58 -0400

    add leetcode 853

---
+++ Java/Medium/853.Car_Fleet/carFleet.java
@@ -0,0 +1,76 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class carFleet {
+
+    /**
+     * N cars are going to the same destination along a one lane road.  The destination is target miles away.
+     * Each car i has a constant speed speed[i] (in miles per hour), and initial position position[i] miles towards the target along the road.
+     * A car can never pass another car ahead of it, but it can catch up to it, and drive bumper to bumper at the same speed.
+     * The distance between these two cars is ignored - they are assumed to have the same position.
+     * A car fleet is some non-empty set of cars driving at the same position and same speed.  Note that a single car is also a car fleet.
+     * If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+     *
+     * How many car fleets will arrive at the destination?
+     *
+     * Note:
+     *
+     * 0 <= N <= 10 ^ 4
+     * 0 < target <= 10 ^ 6
+     * 0 < speed[i] <= 10 ^ 6
+     * 0 <= position[i] < target
+     * All initial positions are different.
+     *
+     * Approach: Sorting
+     * 可以按照初始位置对所有车进行从大到小排序。若后一辆车S可以超过前一辆车F，那么F和S一定可以在某一位置形成一个fleet。若后续仍有车可以赶上这个fleet，那么一定是
+     * 后面的车与可以超过S。
+     *
+     * 如何判断S可以超过F？
+     * 可以先计算出F到达终点所需要的时间t，若后一辆车S可以在t时间内到达大于target的位置，说明S可以在t时间内超过F。因此F和S是同一个fleet。只需遍历整个数组，计算出
+     * fleet总数即可
+     *
+     * Time: O(nlogn) 需要对初始位置进行排序
+     * Space: O(n) 因为需要同时存储位置和速度信息，因此需要重新构建一个二维数组存储该信息，然后进行排序
+     */
+    public int carFleet(int target, int[] position, int[] speed) {
+        int N = position.length;
+        int[][] pairs = new int[N][2];
+        //将位置和速度的信息记录成pair
+        for (int i = 0; i < N; i++) {
+            pairs[i] = new int[]{position[i], speed[i]};
+        }
+        //然后对这些pair按初始位置从大到小排序
+        Arrays.sort(pairs, (a, b) -> {return b[0] - a[0];});
+        int i = 0, count = 0;
+        while (i < N) {
+            count++;
+            //计算当前车（F）到达终点所需要的时间
+            double time = (target - pairs[i][0]) * 1.0 / pairs[i][1];
+            //然后判断下一辆车能否在time时间内也到达终点
+            i++;
+            while (i < N && (pairs[i][0] + time * pairs[i][1] >= target * 1.0)) {
+                //若S能在time时间内到达终点，说明S和F可以构成一个fleet，然后继续判断下一辆车，能否与S形成fleet
+                i++;
+            }
+        }
+        return count;
+    }
+
+    @Test
+    public void carFleetTest() {
+        /**
+         * Example:
+         * Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+         * Output: 3
+         * Explanation:
+         * The cars starting at 10 and 8 become a fleet, meeting each other at 12.
+         * The car starting at 0 doesn't catch up to any other car, so it is a fleet by itself.
+         * The cars starting at 5 and 3 become a fleet, meeting each other at 6.
+         * Note that no other cars meet these fleets before the destination, so the answer is 3.
+         */
+        int[] position = new int[]{10, 8, 0, 5, 3};
+        int[] speed = new int[]{2, 4, 1, 1, 3};
+        assertEquals(3, carFleet(12, position, speed));
+    }
+}
