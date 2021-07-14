8a7bc78a9e1e4327269e4a0186fffbdf0a12826e
ZTong1201 <buaatzy@163.com>
2021-07-13 21:02:54 -0700

    change module to source folder for each problem

--- Java/Hard/212.Word_Search_II/findWords.java
+++ Java/Hard/212.Word_Search_II/findWords.java
@@ -1,22 +1,28 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class findWords {
 
     /**
      * Given a 2D board and a list of words from the dictionary, find all words in the board.
-     *
+     * <p>
      * Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically
      * neighboring. The same letter cell may not be used more than once in a word.
-     *
+     * <p>
      * Note:
-     *
+     * <p>
      * All inputs are consist of lowercase letters a-z.
      * The values of words are distinct.
      */
     public List<String> findWords(char[][] board, String[] words) {
-
+        return null;
     }
 
 
@@ -44,7 +50,7 @@ public class findWords {
         Set<String> expected = new HashSet<>(Arrays.asList("eat", "oath"));
         Set<String> actual = new HashSet<>(findWords(board, words));
         assertEquals(expected.size(), actual.size());
-        for(String word : expected) {
+        for (String word : expected) {
             assertTrue(actual.contains(word));
         }
 
--- Java/Hard/460.LFU_Cache/LFUCache.java
+++ Java/Hard/460.LFU_Cache/LFUCache.java
@@ -1,15 +1,13 @@
-import java.util.*;
-
 public class LFUCache {
 
     /**
      * Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
-     *
+     * <p>
      * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
      * put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should
      * invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie
      * (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
-     *
+     * <p>
      * Follow up:
      * Could you do both operations in O(1) time complexity?
      */
@@ -18,7 +16,7 @@ public class LFUCache {
     }
 
     public int get(int key) {
-
+        return 0;
     }
 
     public void put(int key, int value) {
