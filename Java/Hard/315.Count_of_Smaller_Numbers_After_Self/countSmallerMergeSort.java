1b78ae2c6f9b232c3da2a469b7de8ef8f7ffaaff
ZTong1201 <buaatzy@163.com>
2021-07-16 15:52:34 -0700

    update test name

--- Java/Hard/315.Count_of_Smaller_Numbers_After_Self/countSmallerMergeSort.java
+++ Java/Hard/315.Count_of_Smaller_Numbers_After_Self/countSmallerMergeSort.java
@@ -95,7 +95,7 @@ public class countSmallerMergeSort {
     }
 
     @Test
-    public void countSmallerBSTTest() {
+    public void countSmallerMergeSortTest() {
         /**
          * Input: [5,2,6,1]
          * Output: [2,1,1,0]
