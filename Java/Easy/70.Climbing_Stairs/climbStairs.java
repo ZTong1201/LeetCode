2f1fb5258d6295ca1148511d3acfd5978d95e18a
ZTong1201 <43557597+ZTong1201@users.noreply.github.com>
2021-07-22 13:37:11 -0700

    Optimize solutions to LeetCode 70 (#30)

--- Java/Easy/70.Climbing_Stairs/climbStairs.java
+++ Java/Easy/70.Climbing_Stairs/climbStairs.java
@@ -1,55 +1,77 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class climbStairs {
 
     /**
      * You are climbing a stair case. It takes n steps to reach to the top.
-     *
+     * <p>
      * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-     *
+     * <p>
      * Note: Given n will be a positive integer.
-     *
+     * <p>
      * Approach 1: Brute Force (Recursion)
-     *
+     * <p>
      * Time: O(2^n) recursion tree is of size 2^n
      * Space: O(n) in the worst case, the call stack requires O(n)
      */
     public int climbStairsRecursive(int n) {
-        if(n <= 2) return n;
+        if (n <= 1) return 1;
         return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
     }
 
     /**
-     * Approach 2: Dynamic Programming
-     *
-     *
+     * Approach 2: Recursion with memorization
+     * <p>
+     * Time: O(n)
+     * Space: O(n) need extra space to store visited nodes
+     */
+    public int climbStairsMemorization(int n) {
+        int[] memo = new int[n + 1];
+        return climb(memo, n);
+    }
+
+    private int climb(int[] memo, int n) {
+        if (n <= 1) return 1;
+        // if memo[n] is not 0, which means it's been visited
+        // return the memorized value
+        if (memo[n] != 0) return memo[n];
+        // update memo array
+        memo[n] = climb(memo, n - 1) + climb(memo, n - 2);
+        // return the last value in the array
+        return memo[n];
+    }
+
+    /**
+     * Approach 3: Dynamic Programming
+     * <p>
+     * <p>
      * Time: O(n)
      * Space: O(n)
      */
     public int climbStairsDP(int n) {
-        if(n <= 2) return n;
-        int[] dp = new int[n];
+        int[] dp = new int[n + 1];
         dp[0] = 1;
-        dp[1] = 2;
-        for(int i = 2; i < n; i++) {
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n - 1];
+        return dp[n];
     }
 
     /**
-     * Approach 3: Dynamic Programming without extra space
-     *
+     * Approach 4: Dynamic Programming without extra space
+     * <p>
      * Time: O(n)
      * Space: O(1)
      */
     public int climbStairsDPTwoValues(int n) {
-        if(n <= 2) return n;
+        if (n <= 1) return n;
         int f1 = 1;
-        int f2 = 2;
+        int f2 = 1;
         int res = 0;
-        for(int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             res = f1 + f2;
             f1 = f2;
             f2 = res;
@@ -68,7 +90,8 @@ public class climbStairs {
         assertEquals(2, climbStairsDPTwoValues(2));
         assertEquals(3, climbStairsDPTwoValues(3));
         assertEquals(8, climbStairsDPTwoValues(5));
-
-
+        assertEquals(2, climbStairsMemorization(2));
+        assertEquals(3, climbStairsMemorization(3));
+        assertEquals(8, climbStairsMemorization(5));
     }
 }
