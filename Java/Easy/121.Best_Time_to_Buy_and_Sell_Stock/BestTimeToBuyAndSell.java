faf7bb8edcd2c682d8651acd70163da4c2bc4bfb
ZTong1201 <43557597+ZTong1201@users.noreply.github.com>
2021-07-22 20:49:16 -0700

    Update solution to LeetCode 121 (#35)

--- Java/Easy/121.Best_Time_to_Buy_and_Sell_Stock/BestTimeToBuyAndSell.java
+++ Java/Easy/121.Best_Time_to_Buy_and_Sell_Stock/BestTimeToBuyAndSell.java
@@ -1,48 +1,54 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class BestTimeToBuyAndSell {
 
     /**
      * Say you have an array for which the ith element is the price of a given stock on day i.
-     *
+     * <p>
      * If you were only permitted to complete at most one transaction
      * (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
-     *
+     * <p>
      * Note that you cannot sell a stock before you buy one.
-     *
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= prices.length <= 105
+     * 0 <= prices[i] <= 104
+     * <p>
+     * Approach 1: Finding minimum and maximum in one pass
+     * <p>
      * Actually we're finding the maximum and minimum point in the array. We can solve it by one pass.
-     *
+     * <p>
      * Time: O(N)
      * Space: O(1)
      */
     public int maxProfit(int[] prices) {
         int res = 0;
-        int currMin = Integer.MAX_VALUE;
-        for(int i = 0; i < prices.length; i++) {
-            if(prices[i] < currMin) currMin = prices[i];
-            else if(prices[i] - currMin > res) res = prices[i] - currMin;
+        int currMin = prices[0];
+        for (int price : prices) {
+            if (price < currMin) currMin = price;
+            else if (price - currMin > res) res = price - currMin;
         }
         return res;
     }
 
     /**
-     * We can also use dynamic programming method, update max profit at each price, and then return the final value in the array
-     *
+     * Approach 2: Dynamic Programming
+     * We can also use dynamic programming method, update max profit at each price, and then return the final value
+     * in the array
+     * <p>
      * Time: O(N) still one pass
      * Space: O(1) no extra space needed
      */
     public int maxProfitDP(int[] prices) {
-        int currMin = Integer.MAX_VALUE;
-        for(int i = 0; i < prices.length; i++) {
-            if(prices[i] < currMin) {
-                currMin = prices[i];
-                prices[i] = i == 0 ? 0 : prices[i - 1];
-            } else {
-                prices[i] = Math.max(prices[i - 1], prices[i] - currMin);
-            }
+        int res = 0, currMin = prices[0];
+        for (int price : prices) {
+            res = Math.max(res, price - currMin);
+            currMin = Math.min(price, currMin);
         }
-        return prices.length == 0 ? 0 : prices[prices.length - 1];
+        return res;
     }
 
     @Test
@@ -63,18 +69,11 @@ public class BestTimeToBuyAndSell {
         assertEquals(0, maxProfitDP(prices2));
         /**
          * Example 3:
-         * Input: []
-         * Output: 0
-         */
-        int[] prices3 = new int[0];
-        assertEquals(0, maxProfitDP(prices3));
-        /**
-         * Example 4:
          * Input: [2, 4, 1]
          * Output: 2
          */
-        int[] prices4 = new int[]{2, 4, 1};
-        assertEquals(2, maxProfitDP(prices4));
+        int[] prices3 = new int[]{2, 4, 1};
+        assertEquals(2, maxProfitDP(prices3));
     }
 
 
@@ -96,17 +95,10 @@ public class BestTimeToBuyAndSell {
         assertEquals(0, maxProfit(prices2));
         /**
          * Example 3:
-         * Input: []
-         * Output: 0
-         */
-        int[] prices3 = new int[0];
-        assertEquals(0, maxProfit(prices3));
-        /**
-         * Example 4:
          * Input: [2, 4, 1]
          * Output: 2
          */
-        int[] prices4 = new int[]{2, 4, 1};
-        assertEquals(2, maxProfit(prices4));
+        int[] prices3 = new int[]{2, 4, 1};
+        assertEquals(2, maxProfit(prices3));
     }
 }
