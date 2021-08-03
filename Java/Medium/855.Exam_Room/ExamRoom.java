585f6792d8028d19f1b6a5ef0bd2df2a39841bd3
ZTong1201 <43557597+ZTong1201@users.noreply.github.com>
2021-08-03 13:52:15 -0700

    Update solution to LeetCode 855 (#75)

--- Java/Medium/855.Exam_Room/ExamRoom.java
+++ Java/Medium/855.Exam_Room/ExamRoom.java
@@ -1,30 +1,30 @@
-import java.util.*;
+import java.util.TreeSet;
 
 public class ExamRoom {
 
     /**
      * In an exam room, there are N seats in a single row, numbered 0, 1, 2, ..., N-1.
-     *
+     * <p>
      * When a student enters the room, they must sit in the seat that maximizes the distance to the closest person.  If there are
      * multiple such seats, they sit in the seat with the lowest number.  (Also, if no one is in the room, then the student sits at seat
      * number 0.)
-     *
+     * <p>
      * Return a class ExamRoom(int N) that exposes two functions: ExamRoom.seat() returning an int representing what seat the student sat in,
      * and ExamRoom.leave(int p) representing that the student in seat number p now leaves the room.  It is guaranteed that any calls to
      * ExamRoom.leave(p) have a student sitting in seat p.
-     *
+     * <p>
      * Approach: Tree Set
      * 需要将已经分配好的座位记录下来，当新来一位学生需要分配座位时，需要从小到大依次看两个座位直接的间隔。为了使新加入的学生与其相邻的学生的间隔最大，需要取
      * 两个学生的中间座位分配。因为需要顺序遍历座位序号，考虑使用tree set，在学生离开时，只要将该元素移除即可，时间为O(logn)。在加入新学生时，可以先记录下
      * tree set中的第一个元素，其表示着与最左端座位（即位置0）的距离。因为可能存在新加入的学生坐在端点使得距离最大，坐在端点时，其只有一个相邻学生。然后依次
      * 遍历所有元素，取两两差值的均值，将其视作新加入学生与其相邻学生的最大距离。最终取得距离最大的第一个座位即可。最后仍需考虑，新加入学生坐在右端点的情况，
      * 判断新学生坐在右端点与已分配的最大座位号直接的差值是否能得到更大的距离即可。
-     *
+     * <p>
      * Time: seat() - O(n) 因为分配座位需要遍历所有元素，leave() - O(logn)
      * Space: O(n) 需要将所有已分配座位放入tree set
      */
-    private TreeSet<Integer> seats;
-    private int num;
+    private final TreeSet<Integer> seats;
+    private final int num;
 
     public ExamRoom(int N) {
         this.seats = new TreeSet<>();
