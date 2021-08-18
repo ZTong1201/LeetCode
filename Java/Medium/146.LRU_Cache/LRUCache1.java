70115310a92220c384d6ebd46103e8b9ddc22d03
ZTong1201 <buaatzy@163.com>
2021-08-18 16:13:28 -0700

    Update solution to LeetCode 146

--- Java/Medium/146.LRU_Cache/LRUCache1.java
+++ Java/Medium/146.LRU_Cache/LRUCache1.java
@@ -1,32 +1,29 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LRUCache1 {
     /**
      * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
-     *
+     * <p>
      * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
      * put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
-     *
+     * <p>
      * The cache is initialized with a positive capacity.
-     *
+     * <p>
      * The tricky part for this problem is that we need to track the Least Recently Used (LRU) item. If we never use these items,
      * we simply remove the first added item when exceeds capacity. If we at any time use the item (e.g. get() method), we should
      * rearrange the priority for removal.
-     *
+     * <p>
      * In this implementation, a hash map and a deque are used.
-     *
+     * <p>
      * Time: O(n), for both put() and get(), we need to remove key from deque, it will take O(n) time to complete the process
      * Space: O(2*n), where n is the capacity, we need to store items in a deque and a map
      */
-    private Map<Integer, Integer> map;
-    private Deque<Integer> deque;
-    private int capacity;
-
-    public LRUCache1() {
-        this.map = new HashMap<>();
-        this.deque = new ArrayDeque<>();
-        //this.capacity = capacity;
-    }
+    private final Map<Integer, Integer> map;
+    private final Deque<Integer> deque;
+    private final int capacity;
 
     public LRUCache1(int capacity) {
         this.map = new HashMap<>();
@@ -35,15 +32,15 @@ public class LRUCache1 {
     }
 
     public int get(int key) {
-        if(!deque.contains(key)) return -1;
+        if (!deque.contains(key)) return -1;
         deque.remove(key);
         deque.addFirst(key);
         return this.map.get(key);
     }
 
     public void put(int key, int value) {
-        if(this.deque.contains(key)) this.deque.remove(key);
-        else if(this.map.size() == this.capacity) this.map.remove(this.deque.removeLast());
+        if (this.deque.contains(key)) this.deque.remove(key);
+        else if (this.map.size() == this.capacity) this.map.remove(this.deque.removeLast());
         this.map.put(key, value);
         this.deque.addFirst(key);
     }
--- Java/Medium/146.LRU_Cache/LRUCache2.java
+++ Java/Medium/146.LRU_Cache/LRUCache2.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LRUCache2 {
 
@@ -6,15 +7,19 @@ public class LRUCache2 {
      * In order to implement get and put in O(1) runtime, we are actually implementing a Linked hash map a.k.a. Ordered Dictionary
      * Hash map will give us O(1) runtime for remove(), put(), and get()
      * However, if we want to remove element at any location, we need a linked list, specifically, a doubly linked list.
-     *
+     * <p>
      * Time: O(1), by implementing another doubly linked list, we reduce both runtime to O(1)
      * Space: O(N), we still need a hash map to store key-node pairs, where N is at most the capacity.
      */
-    private class DLL {
+    private static class DLL {
         int val;
         int key;
         DLL prev;
         DLL next;
+
+        DLL() {
+
+        }
 
         DLL(int key, int val) {
             this.key = key;
@@ -22,46 +27,45 @@ public class LRUCache2 {
         }
     }
 
-    private DLL head;
-    private DLL tail;
-    private int capacity;
-    private Map<Integer, DLL> map;
+    private final DLL head;
+    private final DLL tail;
+    private final int capacity;
+    private final Map<Integer, DLL> map;
 
     public LRUCache2(int capacity) {
         this.capacity = capacity;
         this.map = new HashMap<>();
-        this.head = new DLL(0, 0);
-        this.tail = new DLL(0, 0);
+        this.head = new DLL();
+        this.tail = new DLL();
         this.head.next = tail;
         this.tail.prev = head;
     }
 
     public int get(int key) {
         //If key isn't in the map, return -1
-        if(!this.map.containsKey(key)) return -1;
+        if (!this.map.containsKey(key)) return -1;
         //If key is in the map, return its value;
         DLL node = this.map.get(key);
-        int value = node.val;
         //Remove value from current node
         removeNode(node);
         //Add node at the front of the node so that it won't be removed first
-        addNode(node);
-        return value;
+        addFront(node);
+        return node.val;
     }
 
     public void put(int key, int value) {
         //If key is in the map, update the value to that key
-        if(this.map.containsKey(key)) {
+        if (this.map.containsKey(key)) {
             DLL node = this.map.get(key);
-            //Since we use the current node, we remove it from the current position
-            removeNode(node);
             //Assign new value to that key
             node.val = value;
+            //Since we use the current node, we remove it from the current position
+            removeNode(node);
             //Add it back to the front of the list
-            addNode(node);
+            addFront(node);
         } else {
             //If we reach the capacity, we need to remove the LRU item, which is at the tail of the list
-            if(this.capacity == this.map.size()) {
+            if (this.capacity == this.map.size()) {
                 //remove key-node pair in our map
                 this.map.remove(this.tail.prev.key);
                 //remove node at the end of the list
@@ -70,22 +74,21 @@ public class LRUCache2 {
             //Otherwise, we put the key-node pair in the hash map, and add new node at the front of the list
             DLL node = new DLL(key, value);
             this.map.put(key, node);
-            addNode(node);
+            addFront(node);
         }
 
     }
 
 
-
-    private void removeNode(DLL dllNode) {
-        dllNode.prev.next = dllNode.next;
-        dllNode.next.prev = dllNode.prev;
+    private void removeNode(DLL node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
     }
 
-    private void addNode(DLL dllNode) {
-        dllNode.next = head.next;
-        head.next.prev = dllNode;
-        head.next = dllNode;
-        dllNode.prev = head;
+    private void addFront(DLL node) {
+        head.next.prev = node;
+        node.next = head.next;
+        head.next = node;
+        node.prev = head;
     }
 }
