de4ecf407eaa41b67edf097f9d4dc41d72ecfed3
ZTong1201 <43557597+ZTong1201@users.noreply.github.com>
2021-08-18 13:15:15 -0700

    Update solution to LeetCode 445 (#137)

--- Java/Medium/445.Add_Two_Numbers_II/AddTwoNumbersII.java
+++ Java/Medium/445.Add_Two_Numbers_II/AddTwoNumbersII.java
@@ -125,64 +125,51 @@ public class AddTwoNumbersII {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // first need to compute the lengths of two lists
         // such that we know when it's necessary to add values from both lists
-        int len1 = 0, len2 = 0;
-        ListNode ptr1 = l1, ptr2 = l2;
-        while (ptr1 != null) {
-            len1++;
-            ptr1 = ptr1.next;
-        }
-        while (ptr2 != null) {
-            len2++;
-            ptr2 = ptr2.next;
-        }
+        int len1 = getLength(l1), len2 = getLength(l2);
+        // the subsequent logic assumes len1 is always less than or equal to len2
+        // hence, switch l1 and l2 if it violates the assumption
+        if (len1 > len2) return addTwoNumbers(l2, l1);
 
         // first compute the sum of two nodes which are at the same position
         // without taking care of the carry value
         // since the carry value will be handled later - we need to construct the result list
         // in the reverse way now - the carry value can be passed to next value in the next traversal
-        ListNode head = null;
-        ptr1 = l1;
-        ptr2 = l2;
         // example:
         // 3 -> 3 -> 3 + 7 -> 7 => 3 -> 10 -> 10 => 10 -> 10 -> 3
-        while (len1 != 0 && len2 != 0) {
-            // compute the value at current node position
-            int val = 0;
-            // if l1 is longer than l2, then its value would have been carried to this position
-            if (len1 >= len2) {
-                val += ptr1.val;
-                ptr1 = ptr1.next;
-                // decrement len1
-                len1--;
-            }
-            // if l2 is longer than l1, then its value should also be taken into account;
-            if (len2 > len1) {
-                val += ptr2.val;
-                ptr2 = ptr2.next;
-                // decrement len2
-                len2--;
-            }
-            // construct a new list node and insert it to the front
-            ListNode curr = new ListNode(val);
+
+        // first skip extra nodes in l2 to make l1 and l2 have the same length
+        ListNode head = null;
+        for (int i = 0; i < len2 - len1; i++) {
+            ListNode curr = new ListNode(l2.val);
+            // always insert node at the front
             curr.next = head;
             head = curr;
+            l2 = l2.next;
+        }
+        // then add values together without taking care of the carry value
+        for (int i = 0; i < len1; i++) {
+            ListNode curr = new ListNode(l1.val + l2.val);
+            curr.next = head;
+            head = curr;
+            l1 = l1.next;
+            l2 = l2.next;
         }
 
         // now we need to take care of the carry value
         // since in the previous round, the output list was computed in the reverse order
         // this time it's necessary to reverse it back
-        ptr1 = head;
+        ListNode ptr = head;
         head = null;
         int carry = 0;
-        while (ptr1 != null) {
+        while (ptr != null) {
             // construct a new list node by considering the carry value this time
-            ListNode curr = new ListNode((ptr1.val + carry) % 10);
-            carry = (ptr1.val + carry) / 10;
+            ListNode curr = new ListNode((ptr.val + carry) % 10);
+            carry = (ptr.val + carry) / 10;
             // insert to the front of the output list
             curr.next = head;
             head = curr;
             // keep traversing the result list in the previous round
-            ptr1 = ptr1.next;
+            ptr = ptr.next;
         }
         // remember to handle outstanding carry value
         if (carry != 0) {
@@ -191,6 +178,15 @@ public class AddTwoNumbersII {
             head = curr;
         }
         return head;
+    }
+
+    private int getLength(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            res++;
+            head = head.next;
+        }
+        return res;
     }
 
     @Test
