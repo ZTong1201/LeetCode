dd4e0b6a09591e9e315714739ce1ee420562130d
ZTong1201 <buaatzy@163.com>
2021-08-19 17:53:28 -0700

    Update solution to LeetCode 96

--- Java/Medium/96.Unique_Binary_Search_Trees/uniqueBST.java
+++ Java/Medium/96.Unique_Binary_Search_Trees/uniqueBST.java
@@ -1,12 +1,14 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class uniqueBST {
+
     /**
      * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
-     *
+     * <p>
      * Approach 1: Dynamic Programming
-     *
+     * <p>
      * For a given integer sequence 1 ... n, in order to compute the number of unique BSTs, we can actually select each value as a root value,
      * and use the rest of values to construct unique left subtrees and right subtrees. By doing so, we make sure all the BSTs are unique.
      * Hence, we convert the problem into how many unique ways to construct left and right subtrees for a given root node. It sounds like a
@@ -17,15 +19,15 @@ public class uniqueBST {
      * times unique number of right subtree, i.e. unique number of BSTs for 4 nodes and 3 nodes. We can use recursion to keep searching until
      * we reach the base case, i.e. when there is only one node, the number is simply 1. Or we can record the unique number of BSTs for 3 and
      * 4 nodes for quicker information retrieval using an array.
-     *
+     * <p>
      * In summary, we can denote the number of unique BSTs for value n as F(n), and denote number of unique BSTs with root value i given n nodes
      * as G(i, n). Hence F(n) = sum from i = 1 to n G(i, n)
      * And G(i, n) = F(i - 1) * F(n - i)
-     *
+     * <p>
      * We should initialize the array with res[0] = 1 and res[1] = 1 which corresponds to an empty tree or a tree with only the root value
      * (obviously, there is only 1 unique BST)
-     *
-     *
+     * <p>
+     * <p>
      * Time: The total number of iteration = (2 + 3 + 4 + ... + n) = (n + 2)(n - 1)/2, which is O(n^2)
      * Space: O(n) we need an array with size n + 1 to keep track of the number of unique BSTs for a given number of nodes
      */
@@ -33,8 +35,8 @@ public class uniqueBST {
         int[] res = new int[n + 1];
         res[0] = 1;
         res[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            for(int j = 1; j <= i; j++) {
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 res[i] += res[j - 1] * res[i - j];
             }
         }
@@ -43,22 +45,22 @@ public class uniqueBST {
 
     /**
      * Approach 2: Catalan Numbers
-     *
+     * <p>
      * Actually, the number of unique BSTs is the Catalan number. The convenient form for calculating such number is defined as follows:
-     *
+     * <p>
      * C0 = 1, C(n + 1) = 2(2n + 1)/(n + 2) * C(n)
-     *
+     * <p>
      * Hence we can use this simple form to compute the catalan number.
      * The only thing we need to note is that the catalan number can be very big as n increase, which leads to integer overflow in java,
      * we can actually store the result in a long, and convert it back to int when returning the result.
-     *
+     * <p>
      * Time: O(n) only one simple loop now
      * Space: O(1) no extra space required
      */
     public int numTreesCatalan(int n) {
         long res = 1;
-        for(int i = 0; i < n; i++) {
-            res = res * 2 * (2 * i + 1) / (i + 2);
+        for (int i = 0; i < n; i++) {
+            res = res * 2 * (2L * i + 1) / (i + 2);
         }
         return (int) res;
     }
