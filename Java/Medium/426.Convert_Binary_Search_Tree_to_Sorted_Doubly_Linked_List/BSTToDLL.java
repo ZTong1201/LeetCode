81a031aa0dce8d3f661fb7cd68143e63e428cd74
ZTong1201 <43557597+ZTong1201@users.noreply.github.com>
2021-08-19 17:06:15 -0700

    Update solution to LeetCode 426 (#144)

--- Java/Medium/426.Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List/BSTToDLL.java
+++ Java/Medium/426.Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List/BSTToDLL.java
@@ -1,48 +1,49 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class BSTToDLL {
     /**
      * Convert a BST to a sorted circular doubly-linked list in-place.
      * Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
-     *
+     * <p>
      * Let's take the following BST as an example, it may help you understand the problem better:
-     *           4
-     *         /  \
-     *        2    5
-     *       / \
-     *      1   3
+     *            4
+     *          /  \
+     *         2    5
+     *        / \
+     *       1   3
      * We want to transform this BST into a circular doubly linked list. Each node in a doubly linked list has a predecessor and successor.
      * For a circular doubly linked list, the predecessor of the first element is the last element,
      * and the successor of the last element is the first element.
-     *
+     * <p>
      * The figure below shows the circular doubly linked list for the BST above.
      * The "head" symbol means the node it points to is the smallest element of the linked list.
-     *
+     * <p>
      * head --
-     *       |
-     *       1 -->  2 --> 3 --> 4 --> 5
-     *       ^ <--    <--   <--   <-- ^
-     *       |                        |
-     *        ------------------------
+     * |
+     * 1 -->  2 --> 3 --> 4 --> 5
+     * ^ <--    <--   <--   <-- ^
+     * |                        |
+     * ------------------------
      * Specifically, we want to do the transformation in place. After the transformation, the left pointer of the tree node should
      * point to its predecessor, and the right pointer should point to its successor.
      * We should return the pointer to the first element of the linked list.
-     *
+     * <p>
      * Approach 1: Inorder + New Node
      * In order to construct a sorted data structure from a BST, we need to do an inorder traversal. We can construct a new Doubly-linked
      * list (the Node class in this problem), and assign a pointer to the head of the DLL, as we inorder traverse the entire tree. We
      * insert new node to the DLL, and adjust the left and right node
-     *
+     * <p>
      * Time: O(n), we need to traverse all the tree nodes
      * Space: O(H + n), we construct a new DLL, and the call stack require up to the height of tree space. In the worst case, it is O(n),
-     *        and O(logn) on average.
+     * and O(logn) on average.
      */
     private Node head = new Node(0, null, null);   //a dummy node of the head of DLL
     private Node ptr = head;   //assign a pointer to the DLL
 
     public Node treeToDoublyListNewNode(Node root) {
-        if(root == null) return null;
+        if (root == null) return null;
         treeToDoublyListNewNode(root.left);
         insertToDLL(root.val);
         treeToDoublyListNewNode(root.right);
@@ -71,20 +72,22 @@ public class BSTToDLL {
      * tail together to obtain the desired result. The algorithm looks like this:
      * 1. Initialize the first and the last node as null
      * 2. As we inorder traverse the BST, if the last node is null (which means there is nothing in our final DLL), we assign the first
-     *    node to it, and never change the first node, since it records the head of our DLL
+     * node to it, and never change the first node, since it records the head of our DLL
      * 3. If the last node is not null, we simply link the new node with our last node (a.k.a. the current tail of our DLL)
      * 4. Always move the last node one step ahead to keep track of the tail of the DLL
      * 5. When the traversal is done, link the first and the last node together
-     *
+     * <p>
      * Time: O(n) visit all the nodes once
      * Space: O(H), where H is the height of the tree. Since we do it in-place, no extra space for the result DLL. However, the call stack
-     *        requires O(H) space to traverse the entire tree. It will be degenerated to O(N) in the worst case, and will be O(logn) on average
+     * requires O(H) space to traverse the entire tree. It will be degenerated to O(N) in the worst case, and will be O(logn) on average
      */
-    private Node first = null;
-    private Node last = null;
+    private Node first;
+    private Node last;
 
     public Node treeToDoublyListInPlace(Node root) {
-        if(root == null) return null;
+        if (root == null) return null;
+        first = null;
+        last = null;
         helper(root);
         first.left = last;
         last.right = first;
@@ -92,25 +95,43 @@ public class BSTToDLL {
     }
 
     private void helper(Node root) {
-        if(root != null) {
-            //traverse the left
-            helper(root.left);
+        if (root == null) return;
+        //traverse the left
+        helper(root.left);
 
-            if(last != null) {  //which means we have at least one node in our final DLL, link the new node with the last node
-                last.right = root;
-                root.left = last;
-            } else {   //otherwise, there is nothing in the DLL already, simply assign first node to it
-                first = root;
-            }
-            last = root;    //always assign the current node to the last node, i.e. keep track of the tail of the DLL
-
-            //then, traverse the right
-            helper(root.right);
+        if (last != null) {
+            //which means we have at least one node in our final DLL, link the new node with the last node
+            last.right = root;
+            root.left = last;
+        } else {
+            //otherwise, there is nothing in the DLL already, simply assign first node to it
+            first = root;
         }
+        last = root;    //always assign the current node to the last node, i.e. keep track of the tail of the DLL
+
+        //then, traverse the right
+        helper(root.right);
     }
 
     @Test
     public void treeToDoublyListInPlaceTest() {
+        /**
+         * Example:
+         * Input: root = [4,2,5,1,3]
+         * Output: [1,2,3,4,5]
+         *
+         * Explanation: The figure below shows the transformed BST. The solid line indicates the successor relationship,
+         * while the dashed line means the predecessor relationship.
+         *                          4
+         *                         / \
+         *                        2   5
+         *                       / \
+         *                      1   3
+         * =>
+         * 1 -> 2 -> 3 -> 4 -> 5
+         * | <-   <-   <-   <- |
+         * |___________________|
+         */
         Node bst = new Node(4, null, null);
         bst.left = new Node(2, null, null);
         bst.left.left = new Node(1, null, null);
@@ -124,6 +145,11 @@ public class BSTToDLL {
 
     @Test
     public void treeToDoublyListNewNodeTest() {
+        /**
+         * Example:
+         * Input: root = [4,2,5,1,3]
+         * Output: [1,2,3,4,5]
+         */
         Node bst = new Node(4, null, null);
         bst.left = new Node(2, null, null);
         bst.left.left = new Node(1, null, null);
@@ -137,7 +163,7 @@ public class BSTToDLL {
 
     private int[] DLLToArray(Node root, int length) {
         int[] res = new int[length];
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             res[i] = root.val;
             root = root.right;
         }
@@ -145,14 +171,10 @@ public class BSTToDLL {
     }
 
 
-
-
-    private class Node {
+    private static class Node {
         int val;
         Node left;
         Node right;
-
-        public Node() {}
 
         public Node(int _val, Node _left, Node _right) {
             this.val = _val;
