3fbf35b243fa70e756da0770759f41c026e020df
ZTong1201 <43557597+ZTong1201@users.noreply.github.com>
2021-08-30 19:11:48 -0700

    Update solution to LeetCode 951 (#186)

--- Java/Medium/951.Flip_Equivalent_Binary_Trees/flipBinaryTree.java
+++ Java/Medium/951.Flip_Equivalent_Binary_Trees/flipBinaryTree.java
@@ -1,35 +1,45 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 public class flipBinaryTree {
 
     /**
      * For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
-     *
+     * <p>
      * A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
-     *
+     * <p>
      * Write a function that determines whether two binary trees are flip equivalent.  The trees are given by root nodes root1 and root2.
-     *
+     * <p>
      * Note:
-     *
+     * <p>
      * Each tree will have at most 100 nodes.
      * Each value in each tree will be a unique integer in the range [0, 99].
-     *
+     * <p>
      * Approach 1: Recursion
      * 可以用简单的DFS来判断当前节点是否符合条件。有如下几个case：
      * 1. 如果当前两节点都是null，返回true
      * 2. 如果只有一个节点为null，或者当前两节点都不为null，但是节点数值不相等，返回false
      * 3. 如果当前两节点值相等，可以继续向更深的节点判断。
-     *    对于当前的节点，只有两种可能，即该节点的左右子树没有进行flip，或进行了flip。所以判断其当前节点是否flip equivalent时，可以判断两节点的
-     *    左子树 = 左子树，右子树 = 右子树（即没有进行flip），或判断左子树 = 右子树， 右子树 = 左子树（即两子树进行了flip即可）
-     *
+     * 对于当前的节点，只有两种可能，即该节点的左右子树没有进行flip，或进行了flip。所以判断其当前节点是否flip equivalent时，可以判断两节点的
+     * 左子树 = 左子树，右子树 = 右子树（即没有进行flip），或判断左子树 = 右子树， 右子树 = 左子树（即两子树进行了flip即可）
+     * <p>
      * Time: O(min(N1, N2)), 若两个数大小不等，最坏情况下，遍历完更小的子树后就可以判断出两子树是否相等
      * Space: O(min(H1, H2))， 因为进行递归调用，所以call stack需要O(H)空间，同样的，当递归遍历到深度更小的子树的叶子节点时，就无法再继续更深地遍历
      */
     public boolean flipEquivRecursive(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) return true;
-        if(root1 == null || root2 == null || root1.val != root2.val) return false;
+        // base 1: both trees are null - true
+        if (root1 == null && root2 == null) return true;
+        // base 2: only one of the trees is null - false
+        if (root1 == null || root2 == null) return false;
+        // base 3: if two root values are not the same - false
+        if (root1.val != root2.val) return false;
+        // there are two options at a given root node
+        // 1. left1 == left2 && right1 == right2 meaning two subtrees are identical
+        // 2. left1 = right2 && right1 = left2 meaning the subtrees will be identical after flipping
         return (flipEquivRecursive(root1.left, root2.left) && flipEquivRecursive(root1.right, root2.right)) ||
                 (flipEquivRecursive(root1.left, root2.right) && flipEquivRecursive(root1.right, root2.left));
     }
@@ -38,7 +48,7 @@ public class flipBinaryTree {
      * Approach 2: Canonical Traversal
      * 在遍历时，若左节点的数值大于右节点的数值，可以人为的反转遍历（即先遍历右子树，再遍历左子树），按此遍历得到的结果称为canonical representation，
      * 若两个树可以是翻转等价的，那么按此canonical traversal得到的结果一定相同。
-     *
+     * <p>
      * Time: O(max(N1, N2))， 为了得到canonical representation，需要对每个树进行单独遍历，因此runtime由更大的树决定
      * Space: O(N1 + N2), 需要将所有的节点存在单独的list里
      */
@@ -94,7 +104,7 @@ public class flipBinaryTree {
     }
 
 
-    private class TreeNode {
+    private static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
