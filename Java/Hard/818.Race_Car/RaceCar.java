1cad028d3e6b10a1dcd1717cf870e6b4d354b406
ZTong1201 <buaatzy@163.com>
2021-10-03 16:47:45 -0700

    Add DP solution to LeetCode 818

--- Java/Hard/818.Race_Car/RaceCar.java
+++ Java/Hard/818.Race_Car/RaceCar.java
@@ -44,7 +44,7 @@ public class RaceCar {
      * well need to visit all of them to get the shortest length.
      * Space: O(2^T) we have two conditions at each position, hence we might need to store 2 strings for each index
      */
-    public int raceCar(int target) {
+    public int raceCarBFS(int target) {
         Set<String> visited = new HashSet<>();
         // add pos/speed in the set to avoid duplicate visit
         visited.add("0/1");
@@ -86,6 +86,59 @@ public class RaceCar {
         return -1;
     }
 
+    /**
+     * Approach 2: Recursion + Memorization (DP)
+     * First notice that if the target position equals to 2^n - 1, e.g. 1, 3, 7, etc. The best strategy will always be keeping
+     * accelerating n steps. If the target position is not at 2^n - 1. Then we will have two main strategies, assume the target
+     * position is t, where t != 2^n - 1, more specifically, 2^(n - 1) - 1 < t < 2^n - 1
+     * 1. We can reach t with the smallest number of steps by first accelerating n steps, then we're on the right side of t.
+     * We need to make a reverse (R) then accelerate from 1 again to reach the target. Before re-accelerating, we made n A + 1 R
+     * hence, (n + 1) steps already, the rest of distance is 2^n - 1 - t, this is the sub-problem we need to solve, and it
+     * should've been memorized already. In this scenario, dp[t] = n + 1 + dp[2^n - 1 - t]
+     * 2. We can accelerate (n - 1) steps to reach the left side of t. Then in order to reach t, we need to reverse (R) and
+     * accelerate m steps (0 <= m < n - 1, cuz otherwise we will reverse back beyond the 0 point, which is impossible
+     * to obtain a shorter sequence), and then reverse (R) again, and accelerate from 1 until t. After these sequence, the
+     * current position is actually 2^(n - 1) - 1 - (2^m - 1) = 2^(n - 1) - 2^m, and the rest of distance is t - (2^(n - 1) - 2^m).
+     * Therefore, in this scenario, we had (n - 1) A + 1 R + m A + 1 R = n + m + 1 steps already, the total number of steps
+     * is dp[t] = n + m + 1 + dp[t - (2^(n - 1) - 2^m)]
+     * We will take the minimum from both strategies
+     * <p>
+     * Time: O(TlogT) where T is the target value. For each target value, in the best case, we can get the result in O(1) time,
+     * but on average, we need to take m steps where m < log(T + 1), hence in total we have O(TlogT) runtime
+     * Space: O(T + logT) we need O(T) space for the memorization and O(logT) space for the call stack
+     */
+    public int raceCarDP(int target) {
+        // minOperations[i] = the shortest sequence of instructions to reach position i
+        int[] minOperations = new int[target + 1];
+        return findShortestSequence(target, minOperations);
+    }
+
+    private int findShortestSequence(int pos, int[] minOperations) {
+        // if the current position has been calculated - return the result in O(1) time
+        if (minOperations[pos] > 0) return minOperations[pos];
+        // compute the number of accelerates needed to reach or exceed pos
+        int steps = (int) Math.ceil(Math.log(pos + 1) / Math.log(2));
+        // if pos = 2^n - 1, then we don't need extra steps, just keep accelerating from the beginning
+        if ((1 << steps) == pos + 1) {
+            minOperations[pos] = steps;
+            return minOperations[pos];
+        }
+
+        // strategy 1 - go beyond pos and reverse back and accelerate until pos
+        // we need n A + 1 R before getting started, the distance left is 2^n - 1 - pos
+        minOperations[pos] = steps + 1 + findShortestSequence((1 << steps) - 1 - pos, minOperations);
+        // otherwise, we need to stop before exceeding pos, reverse and accelerate for m steps and reverse back
+        // we have (n - 1) A + 1 R + m A + 1 R = n + m + 1 steps and the current position is 2^(n - 1) - 1 - (2^m - 1)
+        // the distance left is pos - (2^(n - 1) - 2^m)
+        for (int reverse = 0; reverse < steps - 1; reverse++) {
+            int currPos = (1 << (steps - 1)) - (1 << reverse);
+            // always need to the minimum at current pos
+            minOperations[pos] = Math.min(minOperations[pos],
+                    steps + reverse + 1 + findShortestSequence(pos - currPos, minOperations));
+        }
+        return minOperations[pos];
+    }
+
     @Test
     public void raceCarTest() {
         /**
@@ -96,7 +149,8 @@ public class RaceCar {
          * The shortest instruction sequence is "AA".
          * Your position goes from 0 --> 1 --> 3.
          */
-        assertEquals(2, raceCar(3));
+        assertEquals(2, raceCarBFS(3));
+        assertEquals(2, raceCarDP(3));
         /**
          * Example 2:
          * Input: target = 6
@@ -105,7 +159,8 @@ public class RaceCar {
          * The shortest instruction sequence is "AAARA".
          * Your position goes from 0 --> 1 --> 3 --> 7 --> 7 --> 6.
          */
-        assertEquals(5, raceCar(6));
+        assertEquals(5, raceCarBFS(6));
+        assertEquals(5, raceCarDP(6));
     }
 
     private static class CarCondition {
